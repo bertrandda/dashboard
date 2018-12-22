a8646d0fe2634d90e17c84cef877c2ccc218c4ec
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-22 21:20:30 +0000

    Deploy bertrandda/dashboard to github.com/bertrandda/dashboard.git:gh-pages

---
+++ precache-manifest.fa3f8d2515788c7d6710fb9f1946416a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86597d30f1d02395c7f8",
+    "url": "/dashboard/static/css/main.289a44f0.chunk.css"
+  },
+  {
+    "revision": "86597d30f1d02395c7f8",
+    "url": "/dashboard/static/js/main.86597d30.chunk.js"
+  },
+  {
+    "revision": "2f5e67345ee6becb490b",
+    "url": "/dashboard/static/js/1.2f5e6734.chunk.js"
+  },
+  {
+    "revision": "81312d4a81e499095a3d",
+    "url": "/dashboard/static/js/runtime~main.81312d4a.js"
+  },
+  {
+    "revision": "cba6264170ae5e61cd21a299cb1b723d",
+    "url": "/dashboard/index.html"
+  }
+];
